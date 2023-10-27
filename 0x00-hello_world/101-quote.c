92e553d93229f95694a0adb3694efd831db1bceb
Dumbuya1475 <dumbuya366@gmail.com>
2023-10-26 17:27:04 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
+#include <stdio.h>
+
 /**
- *  * main - A programme with a new line
- *   * Return: 1 (success)
- *    */
+ * main - A programme with a new line
+ * Return: 1 (success)
+*/
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
