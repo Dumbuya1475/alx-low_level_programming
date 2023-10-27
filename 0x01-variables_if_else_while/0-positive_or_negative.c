503316e56a0cd2e26c30ada88578d299c155d69a
Dumbuya1475 <dumbuya366@gmail.com>
2023-10-27 02:35:17 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-int main() {
-	    
-	    const con1 = 5;
-	        
-	        int int1 = 3;
-		    
-		    int total = con1 + int1;
-		        
-		        printf("Constant %d ", con1);
-			    printf("Interger %d\n", int1);
-			        
-			        printf("Total is %d ", total);
-				    
-				    scanf("Enter name\n");
-				        printf("Your name is");
-					    
-					    return 0;
-					        } 
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+					if (n > 0)
+							{
+									    printf("%d is positive\n", n);
+									    	} else if (n == 0) {
+												    printf("%d is zero", n);
+												    	} else {
+															    printf("%d is negative", n);
+															    	}
+						
+						return (0);
+}
