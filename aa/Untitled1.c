f86ea7532e1c4c49190856e07247bd0b9a51dadf
Mohamed S. Dumbuya <Dumbuya366@gmail.com>
2023-11-30 09:34:29 +0000

    function pointers

---
+++ aa/Untitled1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int main(void)
+{
+    printf("Hello world");
+    return (0);
+}
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
---
+++ aa/printf.h
@@ -0,0 +1,7 @@
+#ifdef PRINTF_H
+#define PRINTF_H
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif
