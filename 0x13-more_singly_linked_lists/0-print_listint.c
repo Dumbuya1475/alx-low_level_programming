c4e2acb59ed8f86323a6f1307171503fa966fb6b
Dumbuya1475 <dumbuya366@gmail.com>
2023-12-12 09:58:57 -0800

    single_linked_list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
+
 /**
  * print_listint - prints the elements in the nodes of listint_t list
  * @h: head of the node
  *
  * Return: returns the number of nodes in the listint_t list
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
@@ -21,4 +21,3 @@ size_t print_listint(const listint_t *h)
 
 	return (i);
 }
-
